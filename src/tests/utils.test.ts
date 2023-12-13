5cf8d46105ee08872cc533b0620a781791219b66
le.michel <ptitet73@gmail.com>
2023-12-13 16:35:49 +0100

    added test for getContentType

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -1,6 +1,7 @@
 import { describe, it } from 'node:test'
 import * as utils from '../lib/utils.js';
 import assert from 'node:assert';
+import { ContentType } from '../lib/types.js';
 
 describe('test of the utils functions', t => {
     describe('isJSON', () => {
@@ -41,6 +42,23 @@ describe('test of the utils functions', t => {
 
         it('should return ANY for other values', () => {
            assert.equal('ANY', utils.parseRequestMethod('not an http method')); 
+        });
+    });
+
+    describe('getContentType', () => {
+        it('should return OctetStream for buffers', () => {
+            let buf = Buffer.from('');
+            assert.strictEqual(ContentType.OctetStream, utils.getContentType(buf));
+        });
+
+        it('should return JSON for JSON strings', () => {
+            let JSONString = JSON.stringify({ object: { with: ['nested', { properties: true }] }, and: { numbers: 10 } });
+            assert.strictEqual(ContentType.JSON, utils.getContentType(JSONString));
+        });
+
+        it('should return Text for other strings', () => {
+            let str = 'some content';
+            assert.strictEqual(ContentType.Text, utils.getContentType(str));
         });
     });
 });
