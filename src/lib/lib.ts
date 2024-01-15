64e45910c3fe1d1cbb1f39d9c1a43b882e14a16a
le.michel <ptitet73@gmail.com>
2024-01-15 22:32:22 +0100

    export Request and Response classes from lib for easier ts usage

--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -1,6 +1,8 @@
 import { HTTPServer } from './classes/httpServer.js';
 import { Router } from './classes/router.js';
+import { Request } from './classes/request.js';
+import { Response } from './classes/response.js';
 import { RequestStatus, HTTPServerEvent } from './types.js';
 
-export { HTTPServer, Router, RequestStatus, HTTPServerEvent }
-export default { HTTPServer, Router, RequestStatus, HTTPServerEvent }
+export { HTTPServer, Router, RequestStatus, HTTPServerEvent, Request, Response }
+export default { HTTPServer, Router, RequestStatus, HTTPServerEvent, Request, Response }
